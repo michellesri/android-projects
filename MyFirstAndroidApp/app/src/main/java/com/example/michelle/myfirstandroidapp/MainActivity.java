4b1ca9adf22cbfce5614c038d191fa6dcd868d75
Michelle Sri <michelle.sri@gmail.com>
2018-07-14 17:46:57 -0700

    added button to print edittext contents

--- MyFirstAndroidApp/app/src/main/java/com/example/michelle/myfirstandroidapp/MainActivity.java
+++ MyFirstAndroidApp/app/src/main/java/com/example/michelle/myfirstandroidapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.michelle.myfirstandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.my_button);
+        final EditText edtText = (EditText) findViewById(R.id.text_input);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println(edtText.getText().toString());
+            }
+        });
     }
+
 }
