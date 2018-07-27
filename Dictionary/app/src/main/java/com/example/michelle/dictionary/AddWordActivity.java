e901124bf66e41d7d52092772df02d563177216b
Michelle Sri <michelle.sri@gmail.com>
2018-07-27 15:56:36 -0700

    creating add word activity so users can add their own word

---
+++ Dictionary/app/src/main/java/com/example/michelle/dictionary/AddWordActivity.java
@@ -0,0 +1,15 @@
+package com.example.michelle.dictionary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import stanford.androidlib.SimpleActivity;
+
+public class AddWordActivity extends SimpleActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_word);
+    }
+}
