d085665af15625ff3b023cd43807a90150edad30
Michelle Sri <michelle.sri@gmail.com>
2018-07-29 16:42:22 -0700

    todo list successfully adding item

---
+++ TodoList/app/src/main/java/com/example/michelle/todolist/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.michelle.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import stanford.androidlib.SimpleActivity;
+
+public class MainActivity extends SimpleActivity {
+
+    private ArrayList<String> todoItems = new ArrayList<>();
+
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
+        ListView listView = $LV(R.id.todo_list);
+        listView.setAdapter(adapter);
+    }
+
+    public void addItemClick(View view) {
+
+        String userInput = $ET(R.id.user_input).getText().toString();
+
+        if (userInput.length() > 0) {
+            adapter.add(userInput);
+        }
+
+        ListView listView = $LV(R.id.todo_list);
+
+    }
+}
